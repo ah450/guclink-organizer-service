a6eaab71a6b7403787ef2220607b739c69f3d11e
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-16 08:44:26 +0200

    configures multi threading

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module OrganizerService
   class Application < Rails::Application
+    Rails.application.config.middleware.delete Rack::Lock
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << "#{Rails.root}/app/exceptions"
     config.api_only = true
