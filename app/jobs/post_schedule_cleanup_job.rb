5bf0e7a601232a9db972630fffedd68943ee19b7
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-23 17:26:01 +0200

    improves PostScheduleCleanupJob to also cleanup slots without student registration

--- app/jobs/post_schedule_cleanup_job.rb
+++ app/jobs/post_schedule_cleanup_job.rb
@@ -7,12 +7,16 @@ class PostScheduleCleanupJob < ActiveJob::Base
   def perform(new_slots, user)
     StudentRegistration.recreate(new_slots, user)
     orphan_slots = ScheduleSlot.find_by_sql([
-      "SELECT slots.* from schedule_slots AS slots INNER JOIN student_registrations AS regs on " +
+      "SELECT slots.* FROM schedule_slots AS slots INNER JOIN student_registrations AS regs ON " +
       "slots.id = regs.schedule_slot_id " +
       "WHERE regs.user_id = ? " +
       "AND slots.id NOT IN (?)",
       user.id, new_slots.map(&:id)
       ])
     orphan_slots.each(&:destroy)
+    ScheduleSlot.find_by_sql([
+      "SELECT schedule_slots.* FROM schedule_slots " +
+      "WHERE NOT EXISTS (SELECT 1 FROM student_registrations WHERE student_registrations.schedule_slot_id = schedule_slots.id)"
+      ]).each(&:destroy)
   end
 end
--- spec/jobs/post_schedule_cleanup_job_spec.rb
+++ spec/jobs/post_schedule_cleanup_job_spec.rb
@@ -21,4 +21,18 @@ RSpec.describe PostScheduleCleanupJob, type: :job do
     expect(new_slots.reduce(true){|m, e| m && e.persisted?}).to be true
     expect(StudentRegistration.where(schedule_slot: new_slots, user: user).count).to eql 3
   end
+
+  it 'removes slots without student registration' do
+    old_old_slots = FactoryGirl.create_list(:schedule_slot, 2)
+    old_slots = FactoryGirl.create_list(:student_registration, 2, user: user).map(&:schedule_slot)
+    new_slots = FactoryGirl.create_list(:schedule_slot, 8)
+    expect{
+      PostScheduleCleanupJob.perform_now(new_slots, user)
+    }.to change(StudentRegistration, :count).by(6).and change(ScheduleSlot, :count).by(-4)
+    expect(StudentRegistration.count).to eql 8
+    expect(ScheduleSlot.count).to eql 8
+    expect(old_slots.reduce(false){|m, e| m && e.persisted?}).to be false
+    expect(new_slots.reduce(true){|m, e| m && e.persisted?}).to be true
+    expect(StudentRegistration.where(schedule_slot: new_slots, user: user).count).to eql 8
+  end
 end
