adc4700e7f0d01057fba4ef96c3c536b905e685f
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-23 14:02:51 +0200

    implements schedule slot factory

--- spec/factories/schedule_slots.rb
+++ spec/factories/schedule_slots.rb
@@ -1,11 +1,25 @@
 FactoryGirl.define do
   factory :schedule_slot do
-    course nil
-    location "MyString"
-    day ""
-    slot_num ""
+    course
+    location "H9"
+    day { rand(7) }
+    slot_num { rand(6) }
     tutorial false
-    lecture false
+    lecture true
     lab false
+    group "L1"
+    name "CSEN 403"
+    factory :tutorial do
+      lecture false
+      tutorial true
+      location "C6.203"
+      group "T9"
+    end
+    factory :lab do
+      lab true
+      lecture false
+      location "C6.204"
+      group "T9"
+    end
   end
 end
